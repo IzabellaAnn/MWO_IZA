defdc8d526c8bdc95b3437104974839a0e478121
pkawalec <pkawalec@d17-427-pc-20.ipa.ii.agh.edu.pl>
2024-01-20 15:22:25 +0100

    Add rockets

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,4 +2,12 @@ public class SomeProgram {
     public static void main(String[] args) {
         System.out.println("We will be Git experts, soon.");
     }
+
+    public void manageTheSatellite() {
+    // TODO implement
+}
+
+  public void manageRockets() {
+    System.out.println("Rockets");
+}
 }
